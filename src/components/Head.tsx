f4cd6b8f21401a95ec58c8a186362b86c7aa8f5b
NonthawatNK <Nonthawt20@gmail.com>
2021-08-07 21:37:18 +0700

    fix lastname

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -10,7 +10,7 @@
         return (
             <div className='flex justify-center items-end space-x-2'>   
                 <span className='text-center italic my-2 text-2xl'>{main}</span>
-                <span className='text-gray-400 italic my-2 text-xl'>by {firstname+" "+{lastname}+" "+id}</span>
+                <span className='text-gray-400 italic my-2 text-xl'>by {firstname+" "+lastname+" "+id}</span>
             </div>
         )
     }
